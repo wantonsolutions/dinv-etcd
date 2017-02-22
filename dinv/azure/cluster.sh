e74d6ec34eb7737c7ba5e6d1a89535f924fb78cf
wantonsolutions <stewbertgrant@gmail.com>
2017-02-21 18:15:42 -0800

    update

--- dinv/azure/cluster.sh
+++ dinv/azure/cluster.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 #stewart-test-1
 LOCALS1=10.0.1.4
 GLOBALS1=52.228.27.112
@@ -18,6 +21,18 @@ AZURENODE=/dinv/azure/node.sh
 
 TEXT=kahn.in
 
+function onall {
+    ssh stewart@$GLOBALS1 -x $1
+    ssh stewart@$GLOBALS2 -x $1
+    ssh stewart@$GLOBALS3 -x $1
+}
+
+if [ "$1" == "-k" ];then
+    echo kill
+    onall "killall etcd"
+    exit
+fi
+
 #Example execute ssh 
 #ssh stewart@13.64.239.61 -x "mkdir test"
 
@@ -25,13 +40,14 @@ TEXT=kahn.in
 #scp stewart@13.64.239.61:/home/stewart/azureinstall.sh astest
 
 
-CLUSTER="--initial-cluster infra0=http://$LOCALS1:2380,infra1=http://$LOCALS2:2380,infra2=http://$LOCALS3:2380"
+CLUSTER="infra0=http://$LOCALS1:2380,infra1=http://$LOCALS2:2380,infra2=http://$LOCALS3:2380"
 ASSERT="$LOCALS1:12000,$LOCALS2:12000,$LOCALS3:12000"
 
 
 
-ssh stewart@$GLOBALS1 -x "$ETCD$AZURENODE"
-#ssh stewart@$GLOBALS1 -x "$ETCD$AZURENODE 0 $GLOBALS1 $LOCALS1 $CLUSTER $ASSERT" 
-echo "ssh stewart@$GLOBALS1 -x $ETCD/$AZURENODE 0 $GLOBALS1 $LOCALS1 $CLUSTER $ASSERT"
-#ssh stewart@$GLOBALS2 -x "$ETCD$AZURENODE 1 $GLOBALS2 $LOCALS2 $CLUSTER $ASSERT" &
-#ssh stewart@$GLOBALS3 -x "$ETCD$AZURENODE 2 $GLOBALS3 $LOCALS3 $CLUSTER $ASSERT" &
+ssh stewart@$GLOBALS1 -x "$ETCD$AZURENODE 0 $GLOBALS1 $LOCALS1 $CLUSTER $ASSERT" &
+ssh stewart@$GLOBALS2 -x "$ETCD$AZURENODE 1 $GLOBALS2 $LOCALS2 $CLUSTER $ASSERT" &
+ssh stewart@$GLOBALS3 -x "$ETCD$AZURENODE 2 $GLOBALS3 $LOCALS3 $CLUSTER $ASSERT" &
+
+
+
