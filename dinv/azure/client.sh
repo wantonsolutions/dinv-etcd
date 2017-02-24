fb70e6a9332480a2caf703f214b23424c8bb2920
wantonsolutions <stewbertgrant@gmail.com>
2017-02-24 15:25:37 -0800

    updating raft for peers

--- dinv/azure/client.sh
+++ dinv/azure/client.sh
@@ -5,9 +5,10 @@ LatOut=latency.txt
 echo "" > $LatOut
 for word in $(<$1)
 do
-
+    echo $word
+    echo $1 $2 $3 $4
     a=$(($(date +%s%N)/1000000))
-    ETCDCTL_API=3 $3 --endpoints=$2 put $i "$word"
+    ETCDCTL_API=3 $3 --endpoints=$2:2379 put $i "$word"
     b=$(($(date +%s%N)/1000000))
     latency=0
     let latatency=b-a
